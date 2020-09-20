739f6cd66c1124ff0f549a0258138a30f9c408d5
sookylee <bbluv_@naver.com>
2020-09-20 11:37:51 +0900

    capsule부분 router 작성

---
+++ server/routes/capsule.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
+
+var cookieParser = require('cookie-parser');
+router.use(cookieParser());
+
+
+
+
+router.get('/write', function(req, res){
+    res.render('writeLetter.html', {
+        session: req.session
+    });
+})
+
+
+router.get('/read', function(req,res){
+    res.render('readLetter.html', {
+        session: req.session
+    });
+})
+
+module.exports = router;
