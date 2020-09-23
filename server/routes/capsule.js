b6d407feae5dffeea08588e14d0ed0914601fc91
sookylee <bbluv_@naver.com>
2020-09-23 11:06:19 +0900

    add time api

--- server/routes/capsule.js
+++ server/routes/capsule.js
@@ -8,7 +8,10 @@ router.use(bodyParser.urlencoded({extended : true}));
 var cookieParser = require('cookie-parser');
 router.use(cookieParser());
 
-
+// for date
+var moment = require('moment');
+require('moment-timezone');
+moment.tz.setDefault("Asia/Seoul");
 
 
 router.get('/write', function(req, res){
@@ -29,5 +32,24 @@ router.get('/read/view', function(req, res){
         session: req.session
     })
 });
+
+//
+router.post('/read', function(req,res){
+    var arr = req.body.boxes;
+    res.render('capsuleShow.html', {
+        session: req.session,
+        capsules: arr
+    })
+});
+
+
+
+// now date 내려주기 --front 쪽에서 ajax로 받음
+router.get('/time', function(req,res){
+    var date = moment().format('YYYY-MM-DD');
+    console.log(date);
+    res.send(date);
+});
+
 
 module.exports = router;
